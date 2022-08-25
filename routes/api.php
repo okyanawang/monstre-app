8344c177a0fa96406589310461d066181d3e9982
Irwanda <irwandaa7@gmail.com>
2022-08-25 18:08:19 +0700

    feat: Login, register & view profile with bearer

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//API route for register new user
+Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+//API route for login user
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    // API route for logout user
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
