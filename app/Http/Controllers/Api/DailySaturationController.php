258aacbf5e555677de16fe45e3de56696d4e1ab0
Irwanda <irwandaa7@gmail.com>
2022-08-26 18:06:45 +0700

    feat: set and get saturation

---
+++ app/Http/Controllers/Api/DailySaturationController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class DailySaturationController extends Controller
+{
+    public function setTodaySaturation(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'bpm' => 'required|integer',
+            'spo2' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = auth()->user();
+        $date = date('Y-m-d');
+        $bpm = $request->bpm;
+        $spo2 = $request->spo2;
+
+        $todaySaturation = DB::table('daily_saturation')->where('user_id', $user->id)->where('date', $date)->first();
+        if ($todaySaturation) {
+            DB::table('daily_saturation')->where('user_id', $user->id)->where('date', $date)->update([
+                'bpm' => $bpm,
+                'spo2' => $spo2,
+            ]);
+        } else {
+            DB::table('daily_saturation')->insert([
+                'user_id' => $user->id,
+                'date' => $date,
+                'bpm' => $bpm,
+                'spo2' => $spo2,
+            ]);
+        }
+
+        return response()->json(['msg' => 'Today\'s Saturation set successfully']);
+    }
+
+    public function getTodaySaturation()
+    {
+        return response()->json(['status' => 'success', 'data' => DB::select('SELECT * FROM daily_saturation WHERE date = CURDATE() AND user_id = ? LIMIT 1', [auth()->user()->id])]);
+    }
+
+    // Get data from start of the week (monday), to end of the week (sunday)
+    public function getWeekSaturation()
+    {
+        return response()->json(['status' => 'success', 'data' => DB::table('daily_saturation')->where('user_id', auth()->user()->id)->whereBetween('date', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->get()]);
+    }
+
+    // Get data from start of the month, to end of the month
+    public function getMonthSaturation()
+    {
+        return response()->json(['status' => 'success', 'data' => DB::table('daily_saturation')->where('user_id', auth()->user()->id)->whereBetween('date', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->get()]);
+    }
+
+    // Get data from start of the year, to end of the year
+    public function getYearSaturation()
+    {
+        return response()->json(['status' => 'success', 'data' => DB::table('daily_saturation')->where('user_id', auth()->user()->id)->whereBetween('date', [Carbon::now()->startOfYear(), Carbon::now()->endOfYear()])->get()]);
+    }
+
+    // Full week (7) days
+    public function getFullWeekSaturation()
+    {
+        return response()->json(['status' => 'success', 'data' => DB::select('SELECT * FROM daily_saturation WHERE user_id = ? AND date BETWEEN CURDATE() - INTERVAL 7 DAY AND CURDATE()', [auth()->user()->id])]);
+    }
+
+    // Full month (30) days
+    public function getFullMonthSaturation()
+    {
+        return response()->json(['status' => 'success', 'data' => DB::select('SELECT * FROM daily_saturation WHERE user_id = ? AND date BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()', [auth()->user()->id])]);
+    }
+
+    // Full year (365) days
+    public function getFullYearSaturation()
+    {
+        return response()->json(['status' => 'success', 'data' => DB::select('SELECT * FROM daily_saturation WHERE user_id = ? AND date BETWEEN CURDATE() - INTERVAL 365 DAY AND CURDATE()', [auth()->user()->id])]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\DailySaturationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,6 +38,25 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     // API route for update user's avatar
     Route::post('/update-avatar', [App\Http\Controllers\Api\UserController::class, 'updateAvatar']);
+
+    Route::group(['prefix' => 'saturation'], function () {
+        // API route for set today's saturation
+        Route::post('/', [DailySaturationController::class, 'setTodaySaturation']);
+        // API route for get today's saturation
+        Route::get('/', [DailySaturationController::class, 'getTodaySaturation']);
+        // API route for get week's saturation
+        Route::get('/get-week', [DailySaturationController::class, 'getWeekSaturation']);
+        // API route for get month's saturation
+        Route::get('/get-month', [DailySaturationController::class, 'getMonthSaturation']);
+        // API route for get year's saturation
+        Route::get('/get-year', [DailySaturationController::class, 'getYearSaturation']);
+        // API route to get full week saturation
+        Route::get('/get-full-week', [DailySaturationController::class, 'getFullWeekSaturation']);
+        // API route to get full month saturation
+        Route::get('/get-full-month', [DailySaturationController::class, 'getFullMonthSaturation']);
+        // API route to get full year saturation
+        Route::get('/get-full-year', [DailySaturationController::class, 'getFullYearSaturation']);
+    });
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
