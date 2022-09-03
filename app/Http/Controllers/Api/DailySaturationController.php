277e4b659dff4c377a72a3ce2888db6061b7a1b5
Irwanda <irwandaa7@gmail.com>
2022-09-03 15:53:51 +0700

    change python to hosting

--- app/Http/Controllers/Api/DailySaturationController.php
+++ app/Http/Controllers/Api/DailySaturationController.php
@@ -26,7 +26,24 @@ class DailySaturationController extends Controller
         $bpm = $request->bpm;
         $spo2 = $request->spo2;
 
-        $process = shell_exec('py ' . app_path() . '/Http/Controllers/fuzzy-stress.py ' . $bpm . ' ' . $spo2);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://irwanda.pythonanywhere.com/?bpm=' . $bpm . '&spo2=' . $spo2,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $process = curl_exec($curl);
+
+        curl_close($curl);
+
         $result = explode(' ', $process);
         $type = 'insert';
 
